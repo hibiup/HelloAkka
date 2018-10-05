3d7e50fe29dd6b8e71982c73ca848a28f9540c19
326487162 <Welcome@661>
2018-10-05 16:49:13 -0400

    Merge Sample 1 and 2, change pi name

--- src/main/scala/com/hibiup/samples/helloakka/Sample_1_ActorHierarchyExperiments.scala
+++ src/main/scala/com/hibiup/samples/helloakka/Sample_1_ActorHierarchyExperiments.scala
@@ -1,7 +1,6 @@
 package com.hibiup.samples.helloakka
 
-
-package Sample_1_ActorHierarchyExperiments {
+package Sample_1_ActorHierarchyExperiment {
     /**
       * Actor 以树状结构管理：https://doc.akka.io/docs/akka/2.5/guide/tutorial_1.html
       *
@@ -10,7 +9,7 @@ package Sample_1_ActorHierarchyExperiments {
       * */
     import akka.actor.{Actor, ActorSystem, Props}
 
-    object ActorHierarchyExperiments {
+    object ActorHierarchyExperiment {
         def apply() = {
             /** 1-1) 初始化一个 Actor system (akka://testSystem/)*/
             val system = ActorSystem("testSystem")
@@ -27,7 +26,6 @@ package Sample_1_ActorHierarchyExperiments {
 
             /** 1-5) 删除节点 */
             firstRef ! "stop"
-
 
             /** 4) 删除 ActorSystem */
             system.terminate()
@@ -54,5 +52,77 @@ package Sample_1_ActorHierarchyExperiments {
         }
         override def preStart(): Unit = println(s"[started] $this")
         override def postStop(): Unit = println(s"[stopped] $this")
+    }
+}
+
+
+package Sample_1_ActorHierarchyExperiment2 {
+    import akka.actor.{Actor, ActorSystem, Props}
+
+    import scala.io.StdIn
+    import scala.util.control.Breaks.{break, breakable}
+
+    object ActorHierarchyExperiment {
+        def apply() = {
+            val system = ActorSystem("testSystem") //  新建一个 akka://testSystem 管理树
+
+            // 为新建一个管理树生成顶级节点. Props 根据参数类型返回 actor 实例.
+            val rootRef = system.actorOf(Props[SupervisingActor], "first-actor")
+            println(s"Root: $rootRef")
+
+            println(">>> Press \"q\" to exit <<<")
+            breakable {
+                while (true) {
+                    StdIn.readLine() match {
+                        case "q" => break()
+                        case s => rootRef ! s // 期待输入: new, stop, fail
+                    }
+                }
+            }
+
+            system.terminate() // 终结整个树，这会导致递归终结根节点以下的所有子节点
+        }
+    }
+
+    class SupervisingActor extends Actor {
+        override def receive: Receive = {
+            case "new" ⇒
+                // 当根节点收到消息时，通过内建的 context 实例添加一个空子节点
+                val childRef = context.actorOf(Props[SupervisedActor], "second-actor")
+                println(s"Second: $childRef")
+            case msg ⇒ context.child("second-actor").get ! msg
+        }
+    }
+
+    class SupervisedActor extends Actor {
+        override def receive: Receive = {
+            case "fail" =>
+                // 模拟失败，actor 失败后会被自动重建。
+                println("supervised actor fails now")
+                throw new Exception("I failed!")
+
+            case "stop" ⇒
+                // actor 主动终结自己的方式。终结之前，它会调用 postStop() 方法
+                println(s"I'm killing myself! $self")
+                context.stop(self)
+        }
+
+        // actor 初创之后，执行之前，会调用该函数
+        override def preStart(): Unit = {
+            println("child started")
+            context.actorOf(Props[GrandChildActor], "second")
+        }
+
+        override def postStop(): Unit = println("child stopped")
+    }
+
+    class GrandChildActor extends Actor {
+        override def preStart(): Unit = println("grand child started")
+
+        override def postStop(): Unit = println("grand child stopped")
+
+        // Actor.emptyBehavior is a useful placeholder when we don't
+        // want to handle any messages in the actor.
+        override def receive: Receive = Actor.emptyBehavior
     }
 }
--- src/test/scala/com/hibiup/samples/helloakka/Sample_1_ActorHierarchyExperiments_test.scala
+++ src/test/scala/com/hibiup/samples/helloakka/Sample_1_ActorHierarchyExperiments_test.scala
@@ -1,10 +1,15 @@
 package com.hibiup.samples.helloakka
 
 import org.scalatest._
-import com.hibiup.samples.helloakka.Sample_1_ActorHierarchyExperiments._
 
 class Sample_1_ActorHierarchyExperiments_test extends FlatSpec {
-    "A Akka actor" should "" in {
-        ActorHierarchyExperiments()
+    "A Akka actor hierarchy experiment" should "" in {
+        import com.hibiup.samples.helloakka.Sample_1_ActorHierarchyExperiment._
+        ActorHierarchyExperiment()
+    }
+
+    "Another Akka actor hierarchy experiment" should "" in {
+        import com.hibiup.samples.helloakka.Sample_1_ActorHierarchyExperiment2._
+        ActorHierarchyExperiment()
     }
 }

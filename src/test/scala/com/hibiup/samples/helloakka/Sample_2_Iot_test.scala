cbde131c295199bc3b1f9c79cc4959cb8d7d9fa4
326487162 <Welcome@661>
2018-10-05 16:10:42 -0400

    Change files name

--- src/test/scala/com/hibiup/samples/helloakka/Sample_2_Iot_test.scala
+++ src/test/scala/com/hibiup/samples/helloakka/Sample_2_Iot_test.scala
@@ -8,7 +8,7 @@ import org.scalatest._
 /**
   * Created by 326487162 on 2018/03/21.
   */
-class DeviceSpec extends TestKit(ActorSystem("TestSpec"))
+class Sample_2_Iot_test extends TestKit(ActorSystem("TestSpec"))
   with ImplicitSender with WordSpecLike with Matchers with BeforeAndAfterAll {
     override def afterAll
     {

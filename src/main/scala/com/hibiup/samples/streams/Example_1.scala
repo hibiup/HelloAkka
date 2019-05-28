5d5412bca3b5b96e4441993b3ad78cec1a6c3893
326487162 <Welcome@661>
2019-05-28 16:52:07 -0400

    Add termination

--- src/main/scala/com/hibiup/samples/streams/Example_1.scala
+++ src/main/scala/com/hibiup/samples/streams/Example_1.scala
@@ -1,5 +1,10 @@
 package com.hibiup.samples.streams
 
+import java.io
+
+import akka.NotUsed
+import akka.stream.scaladsl.Source
+
 import scala.concurrent.duration.Duration
 
 /**
@@ -19,15 +24,20 @@ object Example_1 {
         import akka.util.ByteString
         import scala.concurrent.duration._
 
-        /** 1）初始化一个 Akka System */
+        /**
+          * 1）初始化一个 Akka System
+          * */
         implicit val system = ActorSystem("testSystem") // 隐式生成 ActorRefFactory 的子类 ActorSystem。
 
-        /** 2）基于 ActorSystem 生成 Akka Stream 的执行引擎。 这个引擎将从数据源中读取数据，并将数据传递给执行函数。 */
+        /**
+          * 2）基于 ActorSystem 生成 Akka Stream 的执行引擎。 这个引擎将从数据源中读取数据，并将数据传递给执行函数。
+          * */
         implicit val materializer = ActorMaterializer() // 隐式获得 system。并生成隐式 ActorMeterializer 实例。
 
         implicit val ec = system.dispatcher    // 隐式生成 ExecutionContext(线程池) 的子类 ExecutionContextExecutor
 
-        /** 3）定义一个数据发射源 Source。
+        /**
+          * 3）定义一个数据发射源 Source。
           *
           * 它有两个参数，第一个是数据类型，比如以下这个数据源将产生 1 到 100 整数。
           * 第二个参数任意，可以用于产生一些辅助值，比如可以是数据源的IP地址等相关数据。如果没有可以用 NotUsed
@@ -39,9 +49,10 @@ object Example_1 {
           *
           * 如果确实关心 Flow 内部的值，那么参考 Sample_3_Graph
           * */
-        val source: Source[Int, NotUsed] = Source(1 to 100)
+        def source: Source[Int, NotUsed] = Source(1 to 100)
 
-        /** 4）从数据源中萃取数据并处理。
+        /**
+          * 4）从数据源中萃取数据并处理。
           *
           * 启动数据源工作流的方法都以 run 开头，该方法隐式从上下文中获得 akka stream 的执行引擎。最终返回一个
           * Future。在本例中执行引擎将持续从 source 中读取数据然后传递给第一个参数，第一个参数是将值打印出来。
@@ -52,31 +63,35 @@ object Example_1 {
           *
           * 以下例子由 Source 直接到 Sink
           * */
-        val done: Future[Done] = source.runWith(Sink.foreach(println)) // 隐式获得 materializer 参数
+        def done: Future[Done] = source.runWith(Sink.foreach(println)) // 隐式获得 materializer 参数
 
-        /** 4-1) source， flow 和 sink 都是可以重用的，下面定义一个将数据写入文件的处理流程。重用之前的 Source，经由两个
+        /**
+          * 4-1) source， flow 和 sink 都是可以重用的，下面定义一个将数据写入文件的处理流程。重用之前的 Source，经由两个
           * flow 处理后结果输出到文件 Sink。(更完整的可重用例子参见：https://doc.akka.io/docs/akka/2.5/stream/stream-quickstart.html)
           *
           * */
         // Flow: scan + zip。 scan 方法类似 fold，初始值是 BitInt(1),然后逐个取出 Source 中的元素 next，得到每一级的阶
         //       乘值 acc，将结果集传给下一级 zip Flow。
-        val factorials = source.scan(BigInt(1))((acc, next) ⇒ {/* println(acc, next);*/ acc * next })
+        def factorials = source.scan(BigInt(1))((acc, next) ⇒ {/* println(acc, next);*/ acc * next })
             .zipWith(Source(0 to 100))((num, idx) ⇒ s"$idx! = $num")
         // Flow: map
-        val accent: Future[IOResult] =
+        def accent: Future[IOResult] =
             factorials.map(line ⇒ ByteString(s"$line\r\n"))
                 .runWith{
                     // Sink
                     FileIO.toPath(Paths.get("C:\\\\Temp\\numbers.txt"))
                 }
 
-        /** 5）等待 Stream 处理结束后关闭 Akka System。
+        /**
+          * 5）等待 Stream 处理结束后关闭 Akka System。
           *
           * 关闭方法由 Future 的 onComplete 回调函数执行，回调函数隐式获得 AkkaSystem 的执行上下文。*/
-        val result = Future.sequence(List(done,accent))
+        def result: Future[List[io.Serializable]] = Future.sequence(List(done,accent))
         result.onComplete(_ ⇒ { println("Finish!!");system.terminate() }) // 隐式获得 ec
 
-        /** 6) 因为 onComplete 是异步操作，如果是最后一行，任务可能会被迫终止，因此退出前必须等待 result 完成。*/
+        /**
+          * 6) 因为 onComplete 是异步操作，如果是最后一行，任务可能会被迫终止，因此退出前必须等待 result 完成。
+          * */
         Await.result(result, 10 seconds)
 
         system.terminate()
@@ -109,11 +124,11 @@ object what_is_NotUsed extends App{
         implicit val system = ActorSystem("testSystem")
         implicit val materializer = ActorMaterializer()
 
-        val source = Source(List("a", "b", "c"))
-        val sink = Sink.fold[String, String]("")(_ + _)
+        def source:Source[String, NotUsed] = Source(List("a", "b", "c"))
+        def sink: Sink[String, Future[String]] = Sink.fold[String, String]("")(_ + _)
 
-        val runnable: RunnableGraph[Future[String]] = source.toMat(sink)(Keep.right)
-        val result: Future[String] = runnable.run()
+        def runnable: RunnableGraph[Future[String]] = source.toMat(sink)(Keep.right)
+        def result: Future[String] = runnable.run()
 
         /**
           * 到目前为止，除非你已经了解了 Akka Stream，否则我非常确定你已经把我跟丢了，这却是也是我想要的，这样你才能了解
